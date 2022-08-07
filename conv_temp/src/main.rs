c1354c366f10ee198d8a59e57410051dae8dbfc7
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-08-07 13:02:50 +0100

    practice excercises

--- conv_temp/src/main.rs
+++ conv_temp/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}", convert_to_celcius(32));
+    println!("{}", convert_to_fahrenheit(0));
+}
+
+fn convert_to_celcius(temp: i32) -> i32 {
+    (temp - 32) * (5 / 9)
+}
+
+fn convert_to_fahrenheit(temp: i32) -> i32 {
+    (temp * (9 / 5)) + 32
 }
--- fib/src/main.rs
+++ fib/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}", fib(32));
 }
+
+/* Prefer loops over recursion in rust */
+fn fib(n: i32) -> i32 {
+    if n <= 3 {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
