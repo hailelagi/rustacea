672e281a59417e6aa6b450f5263d9c165e4024fa
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-08-07 15:31:13 +0100

    wtf?

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -10,7 +10,6 @@ fn main() {
 // Rules
 // - Variable Scope
 
-
 // string types
 // char => utf-8 encoded 4 bytes
 // str => stack allocated encoded bytes - string literal
@@ -27,26 +26,55 @@ fn main() {
 // refs can be mutable - but only once.
 // &mut Type
 
-
 fn rule() {
     let mut s = String::from("hello");
     let mut s_2 = String::from("bye");
 
-    do_stuff(&mut s);
-    s_2 = take_stuff(s_2);
+    println!("s is at {:p}", &s);
 
+    do_stuff(&mut s);
+
+    // declared s_2 is moved into take_stuff
+
+    println!("s_2 is at {:p}", &s_2);
+    s_2 = take_stuff(s_2);
+    println!("{:p}", &s_2);
+
+    // This triggers a new heap allocation? why?
     s.push_str("!!!!");
     s_2.push_str("!!!");
+
+    println!("s is finally at {:p}", &s);
+    println!("s 2 is finally at {:p}", &s_2);
     // `drop() is called freeing s and s_2`
 }
 
 fn take_stuff(mut x: String) -> String {
-    x.push_str(" ,world");
+    x.push_str(
+        "
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation
+        a realllyyy loongggg possilbly large string to trigger heap reallocation",
+    );
+
+    println!("{:p}", &x);
     return x;
     // `drop() is called freeing x
 }
 
 fn do_stuff(s: &mut String) {
     s.push_str(" ,world");
-     // `drop() is never called
+    // `drop() is never called
 }
+
+// Slices - https://doc.rust-lang.org/book/ch04-03-slices.html#the-slice-type
+// slices are pointer based views into the len up to cap of an array
