0b43722afb77807772bb7ab8808c836b4a467863
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-08-13 23:53:36 +0100

    feat: minigrep accept cli input

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("searching for {}", query);
+    print!("in {}", file_path)
 }
 
 // Error handling
