c784549077fcea167bb2e5b9ed1829163d6140ca
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-08-21 20:30:28 +0100

    hello server

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,33 @@
+use std:: {
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}
+};
+
 fn main() {
-    println!("hello, world");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("conn established #{:?}", stream);
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+     let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+     let contents = fs::read_to_string("hello.html").unwrap();
+     let l = contents.len();
+
+     let res = format!("{status_line}\r\nContent-Length: {l}\r\n\r\n{contents}");
+
+     stream.write_all(res.as_bytes()).unwrap();
+    println!("Request: #{:#?}", http_request);
 }
