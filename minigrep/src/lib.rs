5652841806db39870cf6184b1edc84d8be5c5606
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-08-21 16:04:59 +0100

    refactor: use iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,6 +1,6 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 // Structural problems fixed
 // - seperation of concerns / single responsibility
@@ -31,7 +31,11 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
             res.push(line);
         }
     }
-    res
+    // OR
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -54,14 +58,28 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(
+        // generic type with trait bounds
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("no query str"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Invalid filepath"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
